7ae96c36b608a4d94f0913d188d9be45a30e178b
daviespr1406 <david.espinosa-r@mail.escuelaing.edu.co>
2025-05-04 17:34:07 -0500

    feat: update MongoDB connection string and specify database name in application properties

---
+++ src/main/java/com/example/edu/eci/Config/DataSeeder.java
@@ -0,0 +1,41 @@
+package com.example.edu.eci.Config;
+
+import com.example.edu.eci.model.*;
+import com.example.edu.eci.model.Class;
+import com.example.edu.eci.repository.*;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+@Component
+public class DataSeeder implements CommandLineRunner {
+
+private final UserRepository userRepo;
+private final ClassRepository classRepo;
+
+public DataSeeder(UserRepository userRepo, ClassRepository classRepo) {
+this.userRepo = userRepo;
+this.classRepo = classRepo;
+}
+
+@Override
+public void run(String... args) {
+if (userRepo.count() == 0) {
+userRepo.save(new User("123", "Juan","Student","12345678", "juan@gmail.com"));
+userRepo.save(new User("456", "Maria", "Student","12345678","maria@mail.com"));
+}
+
+if (classRepo.count() == 0) {
+Class clase = new Class();
+clase.setId("abc123");
+clase.setName("Taller de pintura");
+clase.setStartTime(LocalDate.of(2023, 10, 1));
+clase.setMaxStudents(10);
+clase.setResources(List.of(new Class.Equipment("Pinceles",4), new Class.Equipment("Lienzos",6)));
+classRepo.save(clase);
+}
+}
+}
